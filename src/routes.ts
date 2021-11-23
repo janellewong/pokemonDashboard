f55f339bc1c03a7c0b9f4c549b839e6b1b1704e1
janellewong <janellewong112@gmail.com>
2021-11-22 22:22:36 -0800

    routes for filter, insertion, delete, sort

--- src/routes.ts
+++ src/routes.ts
@@ -9,4 +9,28 @@ router.get('/', async (req, res) => {
   return res.render('index.njk', { items: queryData })
 })
 
+router.get('/filter/gender', async (req, res) => {
+  const db = req.app.locals.database as mysql.Connection
+  const [queryData] = await db.query('SELECT * FROM Pokemon1 WHERE Gender = "male";')
+  return res.render('index.njk', { items: queryData })
+})
+
+router.get('/insertion', async (req, res) => {
+  const db = req.app.locals.database as mysql.Connection
+  const [queryData] = await db.query('INSERT INTO  Pokemon1 (Pokemon ID, Name, Gender, Nature, Level, ItemID, TrainerID, PokedexID, LocationName) VALUES 	(1, "Sparky", "Female", "Brave", 40, 1, 2, 25, "Pallet Town");')
+  return res.render('index.njk', { items: queryData })
+})
+
+router.get('/deletion', async (req, res) => {
+  const db = req.app.locals.database as mysql.Connection
+  const [queryData] = await db.query('DELETE FROM Pokemon1 WHERE Name = "Pikachu";')
+  return res.render('index.njk', { items: queryData })
+})
+
+router.get('/sort', async (req, res) => {
+  const db = req.app.locals.database as mysql.Connection
+  const [queryData] = await db.query('SELECT * FROM Pokemon1 ORDER BY Level ASC;')
+  return res.render('index.njk', { items: queryData })
+})
+
 export default router
